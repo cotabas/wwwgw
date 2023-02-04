63da1853c62d75a514bfda4ff6ba2051ad8f2152
cotabas <cotabas@gmail.com>
2023-02-03 23:24:20 -0800

    associations are hard..

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  has_many :movies_saved, foreign_key: :saved_movie_id
+  has_many :saves, through: :movies_saved, source: :saved
 end
--- app/models/save.rb
+++ app/models/save.rb
@@ -1,4 +1,4 @@
 class Save < ApplicationRecord
-  belongs_to :user, class_name: "User"
+  belongs_to :saved, class_name: "User"
   belongs_to :saved_movie, class_name: "Movie"
 end
