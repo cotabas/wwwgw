648996707ab20073e6fae78702147f5184dced4f
cotabas <cotabas@gmail.com>
2023-02-06 22:44:29 -0800

    set global variables for tmdb poster paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-      @large_poster = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/"
-      @small_poster = "https://www.themoviedb.org/t/p/w150_and_h225_bestv2/"
+      $large_poster = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/"
+      $small_poster = "https://www.themoviedb.org/t/p/w150_and_h225_bestv2/"
 end
