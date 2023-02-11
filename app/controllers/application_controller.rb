45ed741fd9bbd009df44b8657a0174e01034ebb1
cotabas <cotabas@gmail.com>
2023-02-10 21:28:00 -0800

    added streaming service logos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
       $large_poster = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/"
       $small_poster = "https://www.themoviedb.org/t/p/w150_and_h225_bestv2/"
+      $logo = "https://www.themoviedb.org/t/p/original/"
       $streamers = {
         :disney => 337,
         :prime => 9,
