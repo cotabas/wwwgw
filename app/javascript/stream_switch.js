857dc4146fc35cada9a55904f89bdcca162df0d3
cotabas <cotabas@gmail.com>
2023-02-18 21:31:39 -0800

    event listener isn't working correctly,,

--- app/javascript/stream_switch.js
+++ app/javascript/stream_switch.js
@@ -1,3 +1,19 @@
 console.log("Stream switcher loaded");
 
 
+const toggles = window.document.getElementsByClassName("st_switch");
+
+const test = window.document.getElementById("test");
+
+test.addEventListener("click", console.log("df"));
+
+function toggle_stream(index) {
+  console.log(toggles[index]);
+}
+toggles[0].addEventListener("click", toggle_stream(0));
+
+//for (let i = 0; i < toggles.length; i++) {
+//  toggles[i].addEventListener("click", toggle_stream(i));
+//  
+//}
+
