28741257e24a39279fb9bbe30ba5cabd70e33094
cotabas <cotabas@gmail.com>
2023-02-19 21:58:09 -0800

    all the ways I'm trying to get these checkboxes to dynamically change the database are not working, I think I need to read more about turbo

--- app/javascript/stream_switch.js
+++ app/javascript/stream_switch.js
@@ -3,17 +3,26 @@ console.log("Stream switcher loaded");
 
 const toggles = window.document.getElementsByClassName("st_switch");
 
-const test = window.document.getElementById("test");
 
-test.addEventListener("click", console.log("df"));
+for (let i = 0; i < toggles.length; i++) {
+  toggles[i].addEventListener("click", () => {
+    console.log(toggles[i].value);
+    console.log(toggles[i].checked);
+    if (toggles[i].checked) {
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", '/streamers/6', true);
 
-function toggle_stream(index) {
-  console.log(toggles[index]);
+      //Send the proper header information along with the request
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+      xhr.onreadystatechange = () => { // Call a function when the state changes.
+          if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            // Request finished. Do processing here.
+          }
+      }
+      xhr.send("disney=true");
+    }
+  });
+  
 }
-toggles[0].addEventListener("click", toggle_stream(0));
-
-//for (let i = 0; i < toggles.length; i++) {
-//  toggles[i].addEventListener("click", toggle_stream(i));
-//  
-//}
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,15 @@ Rails.application.routes.draw do
   resources :saves
   resources :streamers
   resources :movies
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   root "static#index"
 
-   get '/user/:id', to: 'user#show'
+  get '/user/:id', to: 'user#show'
   get '/search/', to: 'movies#search'
   get '/view_movie/', to: 'movies#view_movie'
 
-  Rails.application.routes.draw do
-  resources :saves
-  resources :streamers
-    devise_for :users, controllers: {
+  devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: "users/sessions"
-    }
-  end
-  # Defines the root path route ("/")
-  # root "articles#index"
+  }
 end
